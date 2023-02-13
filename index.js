2a930ecb6c25409572267833e5a97906bf20e781
Samiul Alim <xo.ot2001@gmail.com>
2023-02-13 18:50:44 +0600

    Fixed a few thing

--- index.js
+++ index.js
@@ -8,24 +8,45 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.set("trust proxy", true);
+app.use(cors());
 
-app.get("/api/whois/:domain", cors(), async (req, res) => {
-  let data = await whois(req.params.domain);
-  res.send(data);
+app.get("/api/whois/:domain", async (req, res) => {
+  const domain = req.params.domain;
+  try {
+    let data = await whois(domain);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
 });
 
-app.get("/api/myip/info", cors(), async (req, res) => {
+app.get("/api/myip/info", async (req, res) => {
   const rIP = req.ip || req.socket.remoteAddress;
   const ip = rIP.replace(/([a-z:])/g, "");
-  let data = await ipInfo(ip);
-  res.send(data);
+  try {
+    let data = await ipInfo(ip);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(401).json({
+      error: err.message,
+    });
+  }
 });
 
-app.get("/api/dns/:domain", cors(), async (req, res) => {
-  let data = await dns(req.params.domain);
-  res.send(data);
+app.get("/api/dns/:domain", async (req, res) => {
+  const domain = req.params.domain;
+  try {
+    let data = await dns(domain);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
 });
 
 app.listen(port, () => {
-  console.log("listening on Port:4000");
+  console.log(`listening on port: ${port}`);
 });
